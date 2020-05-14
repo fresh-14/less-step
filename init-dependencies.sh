70e4e613a115406e2cc84817b2ae0abee4d01594
AleksMikhailenko <maa1404@mail.ru>
2020-05-14 12:56:04 +0300

    fix init-script

--- init-dependencies.sh
+++ init-dependencies.sh
@@ -1,13 +1,17 @@
+pythonpath=/usr/bin/python3
+python=/usr/bin/python3.5
+servpath=/etc/init.d/
+logpath=/home/box/web/log/
 sudo apt-get update
 sudo apt-get install -y python3.5 python3.5-dev
 sudo python3.5 -m pip install Django==2.0
 sudo python3.5 -m pip install mysqlclient
-servpath=/etc/init.d/
-logpath=/home/box/web/log/
+sudo unlink $pythonpath
+sudo ln -s $python $pythonpath
 mkdir $logpath
 touch ${logpath}{access,error}.log
 sudo ${servpath}mysql start
 mysql -u root -e "create database step;"
 mysql -u root -e "grant all privileges on step.* to 'step'@'localhost' with grant option;"
-sudo ${servpath}nginx reload
+sudo ${servpath}nginx restart
 sudo ${servpath}gunicorn restart
